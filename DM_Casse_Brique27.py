33359630ab020c1ce27305f0359d82992796113b
Vincent <81516479+vincent-fantino@users.noreply.github.com>
2022-12-01 11:56:20 +0100

    Update and rename DM_Casse_Brique26.py to DM_Casse_Brique27.py

--- DM_Casse_Brique27.py
+++ DM_Casse_Brique27.py
@@ -51,11 +51,12 @@ def update():
         deplacement_horizontal = 1
     
     if balle_y + 5 == 256 :
+        vies = vies - 1
         balle_y = 128
         balle_x = 128
         deplacement_vertical = 1
         deplacement_horizontal = 0
-        vies = vies - 1
+
         
     if balle_y == 0 :
         deplacement_vertical = 1
@@ -71,15 +72,14 @@ def update():
         deplacement_vertical = -1
         deplacement_horizontal = -1
         
-    if vies == 0 :
-        pyxel.text(128,128,"PERDU", 1)
+
     
 # =========================================================
 # == DRAW
 # =========================================================
 def draw():
     """création des objets (30 fois par seconde)"""
-    global vaisseau_x, vaisseau_y, balle_y, balle_x,deplacement_vertical
+    global vaisseau_x, vaisseau_y, balle_y, balle_x,deplacement_vertical, vies
     # vide la fenetre
     pyxel.cls(0)
 
@@ -93,7 +93,9 @@ def draw():
 
     #briques
     pyxel.rect(25, 25, 25, 17, 1)
-
+    
+    if vies == 0 :
+        pyxel.text(128,128,"PERDU", 1)
 
 
 
